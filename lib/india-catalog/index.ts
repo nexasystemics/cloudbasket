61ce08b4d14803c81b8fd700e65799638e3097d4
nexqon2026 <nexqonholdings@gmail.com>
2026-03-23 01:40:09 +0530

    fix: populate india-catalog index.ts — wire all 1000 products

--- lib/india-catalog/index.ts
+++ lib/india-catalog/index.ts
@@ -1,25 +1,22 @@
-import { IndiaProduct } from "./types"
-import { HUL_PRODUCTS } from "./personal-care/hul"
-import { DABUR_PRODUCTS } from "./personal-care/dabur"
-import { ITC_PRODUCTS, GODREJ_CONSUMER_PRODUCTS } from "./personal-care/itc-godrej"
-import { GODREJ_APPLIANCE_PRODUCTS } from "./home-appliances/godrej"
-import { BAJAJ_PRODUCTS, HAVELLS_PRODUCTS } from "./home-appliances/bajaj-havells"
-import { PHILIPS_PRODUCTS, PRESTIGE_PRODUCTS } from "./home-appliances/philips-prestige"
-import { BOAT_PRODUCTS, NOISE_PRODUCTS } from "./electronics/boat-noise"
-import { REALME_PRODUCTS, SAMSUNG_BUDGET_PRODUCTS } from "./electronics/realme-samsung"
-import { PUMA_PRODUCTS, BIBA_PRODUCTS } from "./fashion/puma-biba"
-import { ITC_FOOD_PRODUCTS, BRITANNIA_PRODUCTS } from "./food-grocery/itc-britannia"
+import { HUL_PRODUCTS } from './personal-care/hul'
+import { DABUR_PRODUCTS } from './personal-care/dabur'
+import { ITC_PRODUCTS, GODREJ_CONSUMER_PRODUCTS } from './personal-care/itc-godrej'
+import { GODREJ_APPLIANCE_PRODUCTS } from './home-appliances/godrej'
+import { BAJAJ_HAVELLS_PRODUCTS } from './home-appliances/bajaj-havells'
+import { PHILIPS_PRESTIGE_PRODUCTS } from './home-appliances/philips-prestige'
+import { BOAT_PRODUCTS, NOISE_PRODUCTS } from './electronics/boat-noise'
+import { REALME_PRODUCTS, SAMSUNG_BUDGET_PRODUCTS } from './electronics/realme-samsung'
+import { PUMA_PRODUCTS, BIBA_PRODUCTS } from './fashion/puma-biba'
+import { ITC_FOOD_PRODUCTS, BRITANNIA_PRODUCTS } from './food-grocery/itc-britannia'
 
-export const INDIA_CATALOG: IndiaProduct[] = [
+export const INDIA_CATALOG = [
   ...HUL_PRODUCTS,
   ...DABUR_PRODUCTS,
   ...ITC_PRODUCTS,
   ...GODREJ_CONSUMER_PRODUCTS,
   ...GODREJ_APPLIANCE_PRODUCTS,
-  ...BAJAJ_PRODUCTS,
-  ...HAVELLS_PRODUCTS,
-  ...PHILIPS_PRODUCTS,
-  ...PRESTIGE_PRODUCTS,
+  ...BAJAJ_HAVELLS_PRODUCTS,
+  ...PHILIPS_PRESTIGE_PRODUCTS,
   ...BOAT_PRODUCTS,
   ...NOISE_PRODUCTS,
   ...REALME_PRODUCTS,
@@ -27,16 +24,5 @@ export const INDIA_CATALOG: IndiaProduct[] = [
   ...PUMA_PRODUCTS,
   ...BIBA_PRODUCTS,
   ...ITC_FOOD_PRODUCTS,
-  ...BRITANNIA_PRODUCTS
+  ...BRITANNIA_PRODUCTS,
 ]
-
-export { HUL_PRODUCTS } from "./personal-care/hul"
-export { DABUR_PRODUCTS } from "./personal-care/dabur"
-export { ITC_PRODUCTS, GODREJ_CONSUMER_PRODUCTS } from "./personal-care/itc-godrej"
-export { GODREJ_APPLIANCE_PRODUCTS } from "./home-appliances/godrej"
-export { BAJAJ_PRODUCTS, HAVELLS_PRODUCTS } from "./home-appliances/bajaj-havells"
-export { PHILIPS_PRODUCTS, PRESTIGE_PRODUCTS } from "./home-appliances/philips-prestige"
-export { BOAT_PRODUCTS, NOISE_PRODUCTS } from "./electronics/boat-noise"
-export { REALME_PRODUCTS, SAMSUNG_BUDGET_PRODUCTS } from "./electronics/realme-samsung"
-export { PUMA_PRODUCTS, BIBA_PRODUCTS } from "./fashion/puma-biba"
-export { ITC_FOOD_PRODUCTS, BRITANNIA_PRODUCTS } from "./food-grocery/itc-britannia"
